2b6219e1e1a77a210418d739b03538a269ee7098
NihalYagmur <NihalYagmur@users.noreply.github.com>
2018-11-04 16:02:39 +0100

    Update and rename nlp.py to python/nlp.py

--- python/nlp.py
+++ python/nlp.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from nltk.tokenize import word_tokenize
 from nltk.corpus import framenet as fn
 
-fn.frames_by_lemma('bake')
+
 i=0
 list = word_tokenize('add boiling water into the cup.')
 postagged =  nltk.pos_tag(list)
