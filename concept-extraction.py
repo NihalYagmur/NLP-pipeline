d78f3a9047ca838a6a447312add8a0040d60a38e
NihalYagmur <NihalYagmur@users.noreply.github.com>
2018-11-04 15:52:24 +0100

    Add files via upload
    Based on ConceptNet

---
+++ concept-extraction.py
@@ -0,0 +1,21 @@
+import nltk
+from pprint import pprint
+from nltk.tokenize import word_tokenize
+from nltk.corpus import framenet as fn
+import requests
+
+i=0
+print ('add boiling water into the cup.')
+list = word_tokenize('add boiling water into the cup.')
+
+link = 'http://api.conceptnet.io/c/en/'
+
+i = 0
+for x in list:
+ for i in range(20):
+    obj = requests.get(link+x).json()
+    obj.keys()
+    print('word:'+x)
+    print(obj['edges'][i])
+
+
