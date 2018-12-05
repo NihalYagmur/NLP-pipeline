02a1e045313207fbb0961d8b7b5154acfbca8e86
NihalYagmur <NihalYagmur@users.noreply.github.com>
2018-12-05 15:18:57 +0100

    Create combination.py

---
+++ python/combination.py
@@ -0,0 +1,8 @@
+
+from itertools import combinations 
+  
+comb = combinations(['add', 'boiling', 'water', 'into', 'the', 'cup'], 3) 
+  
+# Print the obtained combinations 
+for i in list(comb): 
+    print (i) 
